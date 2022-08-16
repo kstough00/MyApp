90ed4dcb3902e69dd1c8a4e97e178df39f3814af
Your Username <you@example.com>
2022-08-16 16:02:41 -0500

    first commit

---
+++ src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//create object
+		//use object
+		
+		BaseballCoach theCoach = new BaseballCoach();
+		
+		System.out.println(theCoach.getDailyWorkout());
+
+	}
+
+}
