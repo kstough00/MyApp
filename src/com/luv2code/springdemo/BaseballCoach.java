266f511d535cfa84a18b722d47a99ef5d6244661
Your Username <you@example.com>
2022-08-31 13:06:09 -0500

    defined new dependency and created contructor injection

--- src/com/luv2code/springdemo/BaseballCoach.java
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -2,8 +2,27 @@ package com.luv2code.springdemo;
 
 public class BaseballCoach implements Coach {
 	
+	
+	//define private field for the dependency
+	private FortuneService fortuneService;
+	
+	//define constructor for dependency injection
+	public BaseballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
+	@Override
 	public String getDailyWorkout() {
 	return "Spend 30 minutes on batting practice";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		//use fortune service to give fortune
+		
+		
+		return fortuneService.getFortune();
 	}
 
 }
