a906b24e77e2e923a79368a65747a41572737336
Your Username <you@example.com>
2022-08-31 13:10:42 -0500

    pushing unsaved files

--- src/com/luv2code/springdemo/HelloSpringApp.java
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -13,6 +13,8 @@ public class HelloSpringApp {
 		
 			//call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		
 		
 			//close the context
 		
