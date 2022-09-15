a7e1afddcd401a3dc52f081a650aa87cded0fbab
Your Username <you@example.com>
2022-09-15 13:10:16 -0500

    added email and team name

--- src/com/luv2code/springdemo/CricutCoach.java
+++ src/com/luv2code/springdemo/CricutCoach.java
@@ -3,9 +3,32 @@ package com.luv2code.springdemo;
 public class CricutCoach implements Coach {
 
 	private FortuneService fortuneService;
+	
+	private String emailAddress;
+	private String team;
 
 	public CricutCoach() {
 		System.out.println("CricutCoach inside");
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("setEmailAddress inside");
+
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("setTeam inside");
+
+		this.team = team;
 	}
 
 	public void setFortuneService(FortuneService fortuneService) {
